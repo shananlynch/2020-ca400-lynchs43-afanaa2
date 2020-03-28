e19bb01be00d794f0006a48d8f9957e7fdc616b7
Shanan <shanan.lynch43@mail.dcu.ie>
2020-03-28 17:10:00 +0000

    Text box englarged

--- src/Mona_WebApp/forms.py
+++ src/Mona_WebApp/forms.py
@@ -2,7 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Length
 
+
 class CodeCompilerForm(FlaskForm):
     validateCode = TextAreaField(
-        'Enter Code', validators=[DataRequired(), Length(max=1000)])
+        'Enter Code', validators=[DataRequired(), Length(max=10000)])
     compileCode = SubmitField('Compile')
--- src/Mona_WebApp/monaFrontEnd.py
+++ src/Mona_WebApp/monaFrontEnd.py
@@ -4,37 +4,18 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
-posts = [
-    {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'First post content',
-        'date_posted': 'April 21, 2018'
-    }
-]
 
-@app.route("/")
-@app.route("/home")
+@app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('home.html', posts=posts)
-
-@app.route("/about")
-def about():
-    return render_template('about.html',title='About')
-
-@app.route("/compiler", methods=['GET','POST'])
-def compiler():
     form = CodeCompilerForm()
     if form.validate_on_submit():
         flash(f'Code is valid {form.validateCode.data}!', 'success')
-    return render_template('compiler.html',title='Mona Compiler', form=form)
+    return render_template('compiler.html', title='Mona Compiler', form=form)
 
+
+@app.route("/documentation")
+def about():
+    return render_template('about.html', title='Documentation')
 
 
 if __name__ == '__main__':
