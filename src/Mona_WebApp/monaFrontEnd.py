d63c18641be4ddeb6bb5d712cc30c3e96c06eac0
lynchs43 <shanan.lynch43@mail.dcu.ie>
2020-04-25 18:07:56 +0100

    compiles code and outputs

--- src/Mona_WebApp/monaFrontEnd.py
+++ src/Mona_WebApp/monaFrontEnd.py
@@ -2,10 +2,19 @@ from flask import Flask, render_template, url_for, flash, redirect
 from forms import CodeCompilerForm
 import os
 
-def compileCode(code):
-    output = str(code)
-    output += str(os.popen('compileCodeBash').read())
-    return output
+rootDirectory = os.path.dirname(__file__)[:-11]
+inputCodeFilePath = os.path.join(rootDirectory, "Mona_Compiler/Lexer_Test/inputCode.mona")
+compilerDirectory = os.path.join(rootDirectory, "Mona_Compiler/")
+
+
+def compileCode(code,compilerDirectory, inputCodeFilePath):
+    with open(inputCodeFilePath, 'w') as file:
+        file.write(str(code))
+    os.chdir(compilerDirectory)
+    os.system("./script")
+    codeOutput = os.popen("java mona Lexer_Test/inputCode.mona").read()
+    return str(codeOutput)
+
 
 app = Flask(__name__)
 
@@ -15,8 +24,8 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 @app.route("/", methods=['GET', 'POST'])
 def home():
     form = CodeCompilerForm()
-    compiledCode = compileCode(form.validateCode.data)
     if form.validate_on_submit():
+        compiledCode = compileCode(form.validateCode.data, compilerDirectory, inputCodeFilePath)
         flash(f'{compiledCode}!', 'success')
     return render_template('compiler.html', title='Mona Compiler', form=form)
 
