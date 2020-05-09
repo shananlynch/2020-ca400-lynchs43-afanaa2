1421fa75fda8388a41c576b93693125961793876
Ala Afana <ala.afana2@mail.dcu.ie>
2020-05-09 23:18:24 +0100

    Ir code test to print int , float , and arithmetic add test

--- src/Mona_Compiler/IrCodeTest1.py
+++ src/Mona_Compiler/IrCodeTest1.py
@@ -1,10 +1,29 @@
 import unittest
 import IrCodeTestRun
 import  pytest
+
 class testRun(unittest.TestCase):
+    def __init__(self, testName, i):
+        super(testRun, self).__init__(testName)  # calling the super class init varies for different python versions.  This works for 2.7
+        self.i = i
+
 
     def test_Ir(self):
-            result = IrCodeTestRun.runProgram(1)
-            self.assertEqual(result, "hello world")
+            result = IrCodeTestRun.runProgram(self.i)
+            if self.i  == 1 :
+                self.assertEqual(result, "hello world")
+            if self.i  == 2 :
+                self.assertEqual(result, "10")
+            if self.i  == 3 :
+                self.assertEqual(result, "10.100000")
+            if self.i  == 4 :
+                self.assertEqual(result, "2")
+
+
+            print(self.i);
 if __name__ == '__main__':
-    unittest.main()
+    suite = unittest.TestSuite()
+    for i in range(1,5):
+        suite.addTest(testRun('test_Ir', i))
+
+    unittest.TextTestRunner(verbosity=2).run(suite)
--- src/Mona_Compiler/IrCodeTestRun.py
+++ src/Mona_Compiler/IrCodeTestRun.py
@@ -1,8 +1,9 @@
 import os
+import subprocess
 def runProgram(num):
-    os.system('java mona Ircode_test/IrcodeTest'+ str(num))
+    command = ('java mona Ircode_test/IrcodeTest'+ str(num))
+    p = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stderr=subprocess.STDOUT, stdout=subprocess.PIPE, close_fds=True)
+    out,err = p.communicate()
     with os.popen('lli irFileName' ) as stream:
         output = stream.read().rstrip()
         return (output)
-
-print (runProgram(1))
